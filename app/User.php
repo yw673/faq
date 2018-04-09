0991a5b01dcb632ab90a092a0c002ff84cb2375a
yangyin <yw673@njit.edu>
2018-04-09 16:18:18 -0400

    Added answer miration, model, and made user seeder and added tests for user profile and answer

--- app/User.php
+++ app/User.php
@@ -57,5 +57,10 @@ class User extends Authenticatable
         return $this->hasMany('App\Question');
     }
 
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
+
 
 }
